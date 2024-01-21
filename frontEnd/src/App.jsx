60616da88f607acce058c2f2de929c4b801a01f1
Lilyana Ivanova <lilyanaivanova2118@gmail.com>
2024-01-21 18:20:08 +0200

    added authentication router

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import Layout from "./components/Layout/Layout";
 import RegisterCompanyPage from "./pages/RegisterCompanyPage/RegisterCompanyPage";
@@ -7,18 +8,32 @@ import CompanyDetailPage from "./pages/CompanyDetailPage/CompanyDetailPage";
 import AddDepartmentPage from "./pages/AddDepartmentPage/AddDepartmentPage";
 import DepartmentDetailPage from "./pages/DepartmentDetailPage/DepartmentDetailPage";
 import AddEmployeePage from "./pages/AddEmployeePage/AddEmployeePage";
+import AuthContext from "./store/AuthContext";
 
 function App() {
+  const authentication = useContext(AuthContext);
+  if (authentication.user === null) {
+    return (
+      <Layout>
+        <Routes>
+          <Route path="/login" element={<div>"Login page"</div>} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </Layout>
+    );
+  }
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/registerCompany" element={<RegisterCompanyPage />} />
         <Route path="/:companyId" element={<CompanyDetailPage />} />
-        <Route path="/adddepartmentpage" element={<AddDepartmentPage />}/>
-        <Route path="/:companyId/:departmentId" element={<DepartmentDetailPage />} />
+        <Route path="/adddepartmentpage" element={<AddDepartmentPage />} />
+        <Route
+          path="/:companyId/:departmentId"
+          element={<DepartmentDetailPage />}
+        />
         <Route path="/addemployeepage" element={<AddEmployeePage />} />
-        
       </Routes>
     </Layout>
   );
